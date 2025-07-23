3db2b2387577fcbd91947365aab7805b80b8ba51
zaqihdyt13 <sakiihdyt13@gmail.com>
2025-07-23 21:48:43 +0700

    setting railway

--- models/db.js
+++ models/db.js
@@ -3,10 +3,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   multipleStatements: true,
   typeCast: function (field, next) {
     if (field.type === "NEWDECIMAL" || field.type === "DECIMAL") {
