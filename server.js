c41bda710da33972bbeb17b914ce517462fd8782
zaqihdyt13 <sakiihdyt13@gmail.com>
2025-07-24 13:25:34 +0700

    cors 2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 // app.use(cors());
 app.use(cors({
-  origin: "https://www.ghazastore.shop/",
+  origin: "https://www.ghazastore.shop",
 }));
 app.use(express.json());
 
